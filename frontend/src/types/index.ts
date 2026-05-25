a682ab2cf7cf0c517420bdadaeb8adf1b00f0654
Grzegorz Wikar <grzegorz.10724908@ltimindtree.com>
2026-05-25 10:42:38 +0200

    Types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,60 @@
+export enum TicketStatus{
+    Open = "Open",
+    InProgres = "InProgres",
+    Resolved = "Resolved",
+    Closed = "Closed"
+}
+
+export enum TicketPriority{
+    Low = "Low",
+    Medium = "Medium",
+    High = "High",
+    Critical = "Critical"
+}
+
+export enum TicketCategory{
+    Hardware = "Hardware",
+    Software = "Software",
+    Network = "Network",
+    Seciurity = "Seciurity",
+    Other = "Other"
+}
+
+export interface TicketHistoryEntry{
+    fieldChanged: string;
+    oldValue: string;
+    newValue: string;
+    changedByName: string;
+    changedAt: string;
+}
+
+export interface Ticket{
+    id: number;
+    title: string;
+    description: string;
+    status: TicketStatus;
+    priority: TicketPriority;
+    category: TicketCategory;
+    createdByUserId: number,
+    createdByName: string,
+    assignedToUserId: number | null,
+    assignedToName: string | null,
+    createdAt: string,
+    resolvedAt: string | null,
+    history: TicketHistoryEntry[];
+}
+
+export interface AuthResponse{
+    token: string;
+    email: string;
+    firstName: string;
+    role: string;
+    expiresAt: string;
+}
+
+export interface CreatedTicketRequest{
+    title: string;
+    description: string,
+    priority: number;
+    category: number;
+}
