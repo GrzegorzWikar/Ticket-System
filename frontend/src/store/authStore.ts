f08634e2793d7ac1e172c498e7b3319d4c5db9b4
Grzegorz Wikar <grzegorz.10724908@ltimindtree.com>
2026-05-25 11:13:55 +0200

    Store

---
+++ frontend/src/store/authStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { AuthResponse } from "../types";
+
+interface AuthState{
+    token: string | null;
+    user: Omit<AuthResponse, "token"> | null;
+    setAuth: (data: AuthResponse) => void;
+    logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => {
+    token: localStorage.getItem("token");
+    user: (() => {
+        const stored = localStorage.getItem("user");
+        return stored ? JSON.parse(stored) : null;
+    })();
+
+    setAuth: (data) =>{
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify({
+            email: data.email,
+            firstName: data.firstName,
+            role: data.role,
+            expiresAt: data.expiresAt
+        }))
+    };
+
+    logout: () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        set({ token: null, user: null})
+    }
+})
